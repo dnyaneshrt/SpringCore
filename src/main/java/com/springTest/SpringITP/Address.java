ada72cfbb26a98f06ca232c17495c51b9b057796
Dnyaneshwar <sartape51@gmail.com>
2023-07-12 21:30:54 +0530

    initial commit

---
+++ src/main/java/com/springTest/SpringITP/Address.java
@@ -0,0 +1,24 @@
+package com.springTest.SpringITP;
+
+public class Address {
+
+	private String pin;
+	private String city;
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	@Override
+	public String toString() {
+		return "Address [pin=" + pin + ", city=" + city + "]";
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
